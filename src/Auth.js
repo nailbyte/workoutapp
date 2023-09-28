2933ca89bb68bf21a42f66441b1bf90e298cc2b1
Junaid <101417109+nailbyte@users.noreply.github.com>
2023-09-28 19:40:38 +0100

    Material UI added
    Skeleton working

--- src/Auth.js
+++ src/Auth.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
-import {signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { auth } from './firebase';  // <-- Important change here
+
+import { Button, TextField, Grid, Typography, Paper } from "@mui/material";
+import { GoogleAuthProvider, signInWithPopup, signInWithEmailAndPassword, signOut } from "firebase/auth";
 
 
 function Auth() {
@@ -16,6 +18,16 @@ function Auth() {
     }
   };
 
+  const handleGoogleSignIn = async () => {
+    const provider = new GoogleAuthProvider();
+    try {
+      await signInWithPopup(auth, provider);
+      alert("Signed in with Google successfully!");
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
   const handleSignOut = async () => {
     try {
       await signOut(auth);
@@ -26,22 +38,65 @@ function Auth() {
   };
 
   return (
-    <div>
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleSignIn}>Sign In</button>
-      <button onClick={handleSignOut}>Sign Out</button>
-    </div>
+    <Paper elevation={3} style={{ padding: "20px" }}>
+      <Typography variant="h5" align="center">
+        Workout App
+      </Typography>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <TextField
+            variant="outlined"
+            required
+            fullWidth
+            label="Email Address"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            variant="outlined"
+            required
+            fullWidth
+            label="Password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            onClick={handleSignIn}
+          >
+            Sign In
+          </Button>
+        </Grid>
+        <Grid item xs={12}>
+          <Button
+            fullWidth
+            variant="contained"
+            color="primary"
+            onClick={handleGoogleSignIn}
+          >
+            Sign In with Google
+          </Button>
+        </Grid>
+        <Grid item xs={12}>
+          <Button
+            fullWidth
+            variant="contained"
+            color="secondary"
+            onClick={handleSignOut}
+          >
+            Sign Out
+          </Button>
+        </Grid>
+      </Grid>
+    </Paper>
   );
 }
 
