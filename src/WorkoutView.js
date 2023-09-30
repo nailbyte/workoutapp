556f9e099fea3fa7991b16e508cb24c7fde9574d
Junaid <101417109+nailbyte@users.noreply.github.com>
2023-09-30 14:01:00 +0100

    Before streamlining FS querying

--- src/WorkoutView.js
+++ src/WorkoutView.js
@@ -2,164 +2,121 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { db } from './firebase';
 import { Button, Grid, Typography, TextField } from "@mui/material";
-import { doc, getDoc, collection, query, where, getDocs } from 'firebase/firestore';
+import { doc, getDoc, collection, query, where, getDocs, orderBy, limit} from 'firebase/firestore';
 import { AuthContext } from './AuthContext';
 
 function WorkoutView() {
-  const [workoutData, setWorkoutData] = useState(null);
   const [lastWorkoutLog, setLastWorkoutLog] = useState(null);
-  const [localUserData, setLocalUserData] = useState(null);
-  const { user: currentUser, handleLogout } = useContext(AuthContext);
+  const [todayWorkoutData, setTodayWorkoutData] = useState(null);
+  const { user, handleLogout } = useContext(AuthContext);
 
   useEffect(() => {
-    // Fetch the user's data based on their UID
-    const fetchUserData = async () => {
-      if (!currentUser || !currentUser.uid) {
-        console.error("currentUser or currentUser.uid is undefined");
-        return;
-      }
-      console.log("Fetching user data...", currentUser.uid);
-      const userDocRef = doc(db, "users", currentUser.uid);
-
-      try {
-        const userSnap = await getDoc(userDocRef);
-        if (userSnap.exists()) {
-          setLocalUserData(userSnap.data());
-        } else {
-          console.error("User document not found!");
-        }
-      } catch (error) {
-        console.error("Error fetching user data: ", error);
-      }
-    };
-
-    fetchUserData();
-  }, [currentUser]);
-
-  useEffect(() => {
-    // Fetch the workout template data for the logged-in user
-    const fetchWorkoutData = async () => {
-      if (!localUserData || !localUserData.currentWorkoutTemplate) {
-        console.error("User data is not available or user's currentWorkoutTemplate is not set.");
-        return;
-      }
-
-      const workoutDocRef = doc(db, "workoutTemplates", localUserData.currentWorkoutTemplate);
-      try {
-        const docSnap = await getDoc(workoutDocRef);
-        if (docSnap.exists()) {
-          setWorkoutData(docSnap.data());
-        } else {
-          console.error("No document found for the given workout ID.");
-        }
-      } catch (error) {
-        console.error("Error fetching workout data: ", error);
-      }
-    };
-
-    fetchWorkoutData();
-  }, [localUserData]);
-
-  useEffect(() => {
-    // Fetch the last workout log for the user based on their workout template
+    console.log("user: ", user);
     const fetchLastWorkoutLog = async () => {
-      if (!localUserData || !currentUser || !currentUser.uid) {
-        console.error("User data or currentUser is not available.");
-        return;
-      }
-
       const q = query(
         collection(db, "workoutLogs"),
-        where("user", "==", currentUser.uid),
-        where("WorkoutTemplate", "==", localUserData.currentWorkoutTemplate)
-        // You may also want to order by date and limit to 1 to get the most recent log
+        where("user", "==", user.uid),
+        // Order by date and limit to 1 to get the most recent log
+        orderBy("date", "desc"),
+        limit(1)
       );
 
       try {
         const querySnapshot = await getDocs(q);
-        const logs = [];
-        querySnapshot.forEach((doc) => {
-          logs.push(doc.data());
-        });
-
-        // For simplicity, I'm taking the first log, but in practice, you'd probably want the most recent log
-        if (logs.length > 0) {
-          setLastWorkoutLog(logs[0]);
+        if (!querySnapshot.empty) {
+          console.log("querySnapshot: ", querySnapshot);
+          setLastWorkoutLog(querySnapshot.docs[0].data());
         }
       } catch (error) {
         console.error("Error fetching last workout log: ", error);
       }
     };
+    console.log("lastWorkoutLog: ", lastWorkoutLog);
+    const fetchTodayWorkoutData = async () => {
+      const currentTemplate = user.currentWorkoutTemplate;
+      console.log("currentTemplate: ", currentTemplate);
+      const workoutDocRef = doc(db, "workoutTemplates", currentTemplate);
+      try {
+        const docSnap = await getDoc(workoutDocRef);
+        if (docSnap.exists()) {
+          const workoutTemplate = docSnap.data();
+          const lastDay = lastWorkoutLog && lastWorkoutLog.day ? lastWorkoutLog.day : "Day0"; // Set default to Day0 to start with Day1
+          const nextDayNumber = ((parseInt(lastDay.replace('Day', '')) + 1 - 1) % Object.keys(workoutTemplate.days).length) + 1;
+          const nextDay = `Day${nextDayNumber}`;
 
-    fetchLastWorkoutLog();
-  }, [localUserData, currentUser]);
-// ... (rest of the WorkoutView component above the return statement)
+          setTodayWorkoutData(workoutTemplate.days[nextDay]);
+        }
+      } catch (error) {
+        console.error("Error fetching today's workout data: ", error);
+      }
+    };
 
-const determineNextWorkoutDay = (lastWorkoutDay, totalDays) => {
-  if (!lastWorkoutDay) return "Day1"; // Default first day
+    if (user) {
+      fetchLastWorkoutLog();
+      fetchTodayWorkoutData();
+    }
+  }, [user, lastWorkoutLog]);
 
-  const dayNumber = parseInt(lastWorkoutDay.replace('Day', ''));
-  const nextDayNumber = (dayNumber % totalDays) + 1; // Cycle back to Day1 after the last day
-
-  return `Day${nextDayNumber}`;
-};
-
-const lastWorkoutDay = lastWorkoutLog ? Object.keys(lastWorkoutLog.exercises)[0] : null;
-const totalWorkoutDays = workoutData && workoutData.days ? Object.keys(workoutData.days).length : 0;
-const todayWorkoutDay = determineNextWorkoutDay(lastWorkoutDay, totalWorkoutDays);
-
-return (
-  <div>
+  return (
+    <div>
+      {/* Section 1: Last Workout */}
       {lastWorkoutLog && (
-          <div>
-              <Typography variant="h5">Last Workout: {lastWorkoutDay}</Typography>
-              {Object.entries(lastWorkoutLog.exercises[lastWorkoutDay] || {}).map(([exercise, { weight, reps }]) => (
-                  <div key={exercise}>
-                      <Typography variant="h6">{exercise}</Typography>
-                      <Typography>Weight: {weight}, Reps: {reps}</Typography>
-                  </div>
+        <div>
+          <Typography variant="h5">Last Workout: {lastWorkoutLog.day}</Typography>
+          {Object.entries(lastWorkoutLog.exercises).map(([exercise, sets]) => (
+            <div key={exercise}>
+              <Typography variant="h6">{exercise}</Typography>
+              {sets.map((set, index) => (
+                <Typography key={index}>Set {index + 1}: Weight: {set.weight}, Reps: {set.reps}</Typography>
               ))}
-          </div>
+            </div>
+          ))}
+        </div>
       )}
 
-      {workoutData && workoutData.days && todayWorkoutDay && (
-          <div>
-              <Typography variant="h5">Today's Workout: {todayWorkoutDay}</Typography>
-              {Object.entries(workoutData.days[todayWorkoutDay] || {}).map(([exercise, { sets, reps }]) => (
-                  <div key={exercise}>
-                      <Typography variant="h6">{exercise}</Typography>
-                      <Typography>Sets: {sets}, Reps: {reps}</Typography>
-                      <TextField
-                          label="Weight"
-                          variant="outlined"
-                          margin="normal"
-                          fullWidth
-                      />
-                      <TextField
-                          label="Reps"
-                          variant="outlined"
-                          margin="normal"
-                          fullWidth
-                      />
-                  </div>
+      {/* Section 2: Today's Workout Input Fields */}
+      {todayWorkoutData && (
+        <div>
+          <Typography variant="h5">Today's Workout</Typography>
+          {Object.entries(todayWorkoutData).map(([exercise, { sets, reps }]) => (
+            <div key={exercise}>
+              <Typography variant="h6">{exercise}</Typography>
+              {[...Array(sets)].map((_, index) => (
+                <div key={index}>
+                  <Typography>Set {index + 1}</Typography>
+                  <TextField
+                    label="Weight"
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
+                  />
+                  <TextField
+                    label={`Reps (suggested: ${reps})`}
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
+                  />
+                </div>
               ))}
-          </div>
+            </div>
+          ))}
+        </div>
       )}
 
       <Grid container spacing={2}>
-          <Grid item xs={12}>
-              <Button
-                  fullWidth
-                  variant="contained"
-                  color="secondary"
-                  onClick={handleLogout}
-              >
-                  Logout
-              </Button>
-          </Grid>
+        <Grid item xs={12}>
+          <Button
+            fullWidth
+            variant="contained"
+            color="secondary"
+            onClick={handleLogout}
+          >
+            Logout
+          </Button>
+        </Grid>
       </Grid>
-  </div>
-);
+    </div>
+  );
 }
 
 export default WorkoutView;
