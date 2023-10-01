fcab5f3bcea41f83b38462283c24ac4c89a5646a
Junaid <101417109+nailbyte@users.noreply.github.com>
2023-10-02 00:24:51 +0100

    Added exercise dropdown
    Next: Fixed the bug of erroneous default values

--- src/components/CreateProgram/ExerciseComponent.js
+++ src/components/CreateProgram/ExerciseComponent.js
@@ -1,11 +1,13 @@
-import React from "react";
-import { TextField, Card, CardContent, IconButton } from "@mui/material";
+import React, { useState } from "react";
+import { TextField, Card, CardContent, IconButton, Autocomplete } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
-
+import exerciseList from '../../utils/ExerciseList';
 import CustomTextField from '../common/CustomTextField';
 
 const ExerciseComponent = ({ sets, updateSets, exerciseNumber }) => {
   
+  const [selectedExercise, setSelectedExercise] = useState(null);
+
   const isWeightValid = (weight) => {
     return /^(\d+|\d+\.5)$/.test(weight) && weight <= 999;
   };
@@ -22,33 +24,92 @@ const ExerciseComponent = ({ sets, updateSets, exerciseNumber }) => {
     updateSets(exerciseNumber, newSets);
   };
 
+  const exerciseOptions = Object.keys(exerciseList);
+
   return (
     <Card variant="outlined" style={{ marginBottom: "16px" }}>
       <CardContent>
         <h3>Exercise {exerciseNumber + 1}:</h3>
+        <Autocomplete
+          options={exerciseOptions}
+          value={selectedExercise}
+          onChange={(event, newValue) => setSelectedExercise(newValue)}
+          renderInput={(params) => (
+            <TextField {...params} label="Exercise" variant="outlined" />
+          )}
+        />
         {sets.map((set, index) => (
-          <div key={index} style={{ display: "flex", alignItems: "center", marginBottom: "8px" }}>
+          <div
+            key={index}
+            style={{
+              display: "flex",
+              alignItems: "center",
+              marginBottom: "8px",
+            }}
+          >
             Set {index + 1}:
-            <CustomTextField
-              type="number"
-              inputProps={{ step: "0.5", min: "0", max: "999" }}
-              value={set.weight}
-              onChange={(e) => handleInputChange(index, "weight", e.target.value)}
-            />
-            Kg X
-            <CustomTextField
-              type="number"
-              inputProps={{ min: "0", max: "999" }}
-              value={set.reps}
-              onChange={(e) => handleInputChange(index, "reps", e.target.value)}
-            />
-            reps
-            <IconButton onClick={() => updateSets(exerciseNumber, sets.filter((_, sIndex) => sIndex !== index))}>
+            {selectedExercise && exerciseList[selectedExercise].weighted && (
+              <>
+                <CustomTextField
+                  type="number"
+                  inputProps={{ step: "0.5", min: "0", max: "999" }}
+                  value={set.weight}
+                  onChange={(e) =>
+                    handleInputChange(index, "weight", e.target.value)
+                  }
+                />{" "}
+                Kg
+              </>
+            )}
+            {selectedExercise && exerciseList[selectedExercise].reps && (
+              <>
+                X{" "}
+                <CustomTextField
+                  type="number"
+                  inputProps={{ min: "0", max: "999" }}
+                  value={set.reps}
+                  onChange={(e) =>
+                    handleInputChange(index, "reps", e.target.value)
+                  }
+                />{" "}
+                reps
+              </>
+            )}
+            {selectedExercise && exerciseList[selectedExercise].timed && (
+              <>
+                For{" "}
+                <CustomTextField
+                  type="number"
+                  inputProps={{ min: "0" }}
+                  value={set.time}
+                  onChange={(e) =>
+                    handleInputChange(index, "time", e.target.value)
+                  }
+                />{" "}
+                sec
+              </>
+            )}
+            <IconButton
+              onClick={() =>
+                updateSets(
+                  exerciseNumber,
+                  sets.filter((_, sIndex) => sIndex !== index)
+                )
+              }
+            >
               <DeleteIcon fontSize="inherit" />
             </IconButton>
           </div>
         ))}
-        <button onClick={() => updateSets(exerciseNumber, [...sets, { weight: 10, reps: 10 }])}>
+        <button
+          onClick={() => {
+            if (selectedExercise && exerciseList[selectedExercise].timed) {
+              updateSets(exerciseNumber, [...sets, { time: 30 }]);
+            } else {
+              updateSets(exerciseNumber, [...sets, { weight: 10, reps: 12 }]);
+            }
+          }}
+        >
           Add Next Set
         </button>
       </CardContent>
